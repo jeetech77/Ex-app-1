47975b86905f29d1480e67cd9eb9234058cbd4c7
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-06-02 20:29:09 +0530

    refactor : Space and lines

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const textDiv=document.querySelector(".container");
-const btnIncrease=document.querySelector(".btn-increase");
+const textDiv = document.querySelector(".container");
+const btnIncrease = document.querySelector(".btn-increase");
 const btnDecrease = document.querySelector(".btn-decrease");
-btnIncrease.addEventListener('click',()=>{
+btnIncrease.addEventListener('click', () => {
     let currentFontSize = window.getComputedStyle(textDiv).fontSize;
-    currentFontSize=parseInt(currentFontSize)  // Changing  value into Integer
-    currentFontSize+=2; // Increasing font size by 2px
-    textDiv.style.fontSize=`${currentFontSize}px`; // Setting font size using template-literal
+    currentFontSize = parseInt(currentFontSize)  // Changing  value into Integer
+    currentFontSize += 2; // Increasing font size by 2px
+    textDiv.style.fontSize = `${currentFontSize}px`; // Setting font size using template-literal
 })
-btnDecrease.addEventListener('click',()=>{
+btnDecrease.addEventListener('click', () => {
     let currentFontSize = window.getComputedStyle(textDiv).fontSize;
-    currentFontSize=parseInt(currentFontSize)  // Changing  value into Integer
-    currentFontSize-=2; // Decreaseing font size by 2px
-    textDiv.style.fontSize=`${currentFontSize}px`; // Setting font size using template-literal
+    currentFontSize = parseInt(currentFontSize)  // Changing  value into Integer
+    currentFontSize -= 2; // Decreaseing font size by 2px
+    textDiv.style.fontSize = `${currentFontSize}px`; // Setting font size using template-literal
 
 })
