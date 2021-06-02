2d73e910249067a813437193f97b733d1f316fd4
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-06-02 20:25:23 +0530

    Feat : Decrease Button
    Now you can also decrease the font-size by clicking the decrease button.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const textDiv=document.querySelector(".container");
 const btnIncrease=document.querySelector(".btn-increase");
+const btnDecrease = document.querySelector(".btn-decrease");
 btnIncrease.addEventListener('click',()=>{
-    var currentFontSize = window.getComputedStyle(textDiv).fontSize;
+    let currentFontSize = window.getComputedStyle(textDiv).fontSize;
     currentFontSize=parseInt(currentFontSize)  // Changing  value into Integer
     currentFontSize+=2; // Increasing font size by 2px
     textDiv.style.fontSize=`${currentFontSize}px`; // Setting font size using template-literal
-    console.log(currentFontSize);
+})
+btnDecrease.addEventListener('click',()=>{
+    let currentFontSize = window.getComputedStyle(textDiv).fontSize;
+    currentFontSize=parseInt(currentFontSize)  // Changing  value into Integer
+    currentFontSize-=2; // Decreaseing font size by 2px
+    textDiv.style.fontSize=`${currentFontSize}px`; // Setting font size using template-literal
+
 })
